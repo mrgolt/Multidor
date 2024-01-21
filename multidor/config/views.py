e4f2ac081d19107e6b44b8015060557d8c9df1e4
v <mrgolt404@gmail.com>
2024-01-21 21:25:47 +0700

    fix urls app config

--- multidor/config/views.py
+++ multidor/config/views.py
@@ -3,9 +3,9 @@ from django.views.static import serve
 from django.shortcuts import render
 import os
 
-def custom_serve(request, path, document_root=None, show_indexes=False):
+def custom_serve(request, document_root=None, show_indexes=False):
     domain = request.META.get('HTTP_HOST', '')
-    file_path = os.path.join(settings.STATICFILES_DIRS[0], domain, path)
+    file_path = os.path.join(settings.STATICFILES_DIRS[0], domain)
     if path.endswith('.css') and os.path.exists(file_path):
         return serve(request, file_path, document_root, show_indexes)
     else:
