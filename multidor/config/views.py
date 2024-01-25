712614507b2b33727e98b9ab70c249fd2267593f
v <mrgolt404@gmail.com>
2024-01-25 23:05:15 +0700

    add nginx

--- multidor/config/views.py
+++ multidor/config/views.py
@@ -4,18 +4,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import os
 
-def custom_serve(request, document_root=None, show_indexes=False):
-    print(request)
-    path = request.path
+def custom_serve(request):
     domain = request.META.get('HTTP_HOST', '')
-    file_path = os.path.join(settings.STATICFILES_DIRS[0], domain, path)
-    _, extension = os.path.splitext(file_path)
-    if extension:
-        if os.path.exists(file_path):
-            return serve(request, file_path, document_root, show_indexes)
-        else:
-            return HttpResponse(status=404)
-    else:
-        template_path = os.path.join(domain, 'main.html')
-        return render(request, template_path)
 
+    template_path = os.path.join(domain, 'main.html')
+    return render(request, template_path)
