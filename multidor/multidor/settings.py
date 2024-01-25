cf0abe738e757a421aafd3165bc436416dea4430
v <mrgolt404@gmail.com>
2024-01-25 22:32:23 +0700

    add nginx

--- multidor/multidor/settings.py
+++ multidor/multidor/settings.py
@@ -71,6 +71,20 @@ TEMPLATES = [
     },
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
+
 WSGI_APPLICATION = 'multidor.wsgi.application'
 
 
