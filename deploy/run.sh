fc2262bc30db5bb126546dcba62f414a8a1e19c5
v <mrgolt404@gmail.com>
2024-02-01 13:05:47 +0700

    test deploy

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,29 +1,22 @@
-git pull
+#!/usr/bin/env sh
 
-# Проверка успешности git pull
+git pull
 if [ $? -ne 0 ]; then
     echo "Произошла ошибка при выполнении git pull"
     exit 1
 fi
 
-# Шаг 2: Сборка Docker-образов
 docker-compose build
-
-# Проверка успешности сборки
 if [ $? -ne 0 ]; then
     echo "Произошла ошибка при выполнении docker-compose build"
     exit 1
 fi
 
-# Шаг 3: Запуск Docker-контейнеров
 docker-compose up -d
-
-# Проверка успешности запуска
 if [ $? -ne 0 ]; then
     echo "Произошла ошибка при выполнении docker-compose up -d"
     exit 1
 fi
 
-curl -X POST -H 'Content-type: application/json' --data '{"text":"New deploy from Git is successful"}' https://hooks.slack.com/services/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
-
+curl -X POST -H 'Content-type: application/json' --data '{"text":"https://hooks.slack.com/services/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"}'
 echo "Все шаги завершены успешно"
