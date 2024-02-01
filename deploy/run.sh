53c1c2d55dac5c27b01e5e2a316d067b1644fb5d
v <mrgolt404@gmail.com>
2024-02-01 11:13:37 +0700

    test deploy

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,34 +1,11 @@
 #!/usr/bin/env sh
 
-# variables
-GIT_REF=${1}
-GIT_DIR=${2}
-PUID=${3}
-PGID=${4}
+cd ..
 
-# log date
-date
+git pull
 
-# install git
-if ! command -v git > /dev/null 2>&1; then
-    apk add --no-cache \
-        git
+if [ $? -eq 0 ]; then
+  echo "Готово"
+else
+  echo "Произошла ошибка при выполнении git pull"
 fi
-
-# install openssh
-if ! command -v ssh > /dev/null 2>&1; then
-    apk add --no-cache \
-        openssh
-fi
-
-# allow git with different ownership
-git config --global --add safe.directory ${GIT_DIR}
-
-# fetch from git
-git fetch --all
-
-# checkout git reference
-git checkout --force ${GIT_REF}
-
-# set ownership
-chown -R ${PUID}:${PGID} ${GIT_DIR}
