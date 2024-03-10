e3c144f3f913d8b65a875973e64893e760d78279
v <mrgolt404@gmail.com>
2024-03-10 22:59:35 +0700

    test deploy

--- multidor/config/views.py
+++ multidor/config/views.py
@@ -1,16 +1,13 @@
 from django.shortcuts import render
 import os
-from .models import Sites
+from config.models import Sites
 
 def custom_serve(request):
     domain = request.META.get('HTTP_HOST', '')
     try:
         site = Sites.objects.get(allowed_domain=domain)
-        print(domain)
     except Sites.DoesNotExist:
         site = None
-
-    print(site)
 
     template_path = os.path.join(domain, 'main.html')
     return render(request, template_path, {'site': site})
