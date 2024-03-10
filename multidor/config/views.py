9a6314bdde98a6791fab3374ed31ce52f6c53f8a
v <mrgolt404@gmail.com>
2024-03-10 22:33:02 +0700

    test deploy

--- multidor/config/views.py
+++ multidor/config/views.py
@@ -5,9 +5,9 @@ from .models import Sites
 def custom_serve(request):
     domain = request.META.get('HTTP_HOST', '')
     try:
-        site_obj = Sites.objects.get(allowed_domain=domain)
+        site = Sites.objects.get(allowed_domain=domain)
     except Sites.DoesNotExist:
         pass
 
     template_path = os.path.join(domain, 'main.html')
-    return render(request, template_path, {'site': site_obj})
+    return render(request, template_path, {'site': site})
