d4c22deff7843d92a6fa050bd198b5eba673fe21
v <mrgolt404@gmail.com>
2024-03-10 22:36:36 +0700

    test deploy

--- multidor/config/views.py
+++ multidor/config/views.py
@@ -8,7 +8,7 @@ def custom_serve(request):
         site = Sites.objects.get(allowed_domain=domain)
         print(domain)
     except Sites.DoesNotExist:
-        pass
+        site = None
 
     template_path = os.path.join(domain, 'main.html')
     return render(request, template_path, {'site': site})
