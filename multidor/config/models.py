a313dd16a56f2b5e285dcfa75349909613d6be90
v <mrgolt404@gmail.com>
2024-03-10 20:47:12 +0700

    test deploy

--- multidor/config/models.py
+++ multidor/config/models.py
@@ -4,7 +4,20 @@ class Sites(models.Model):
     site_id = models.AutoField(primary_key=True)
     allowed_domain = models.CharField(max_length=255)
 
-class Templates(models.Model):
-    template_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    template_file = models.FileField(upload_to='template_files/', null=True, blank=True)
+class Casino(models.Model):
+    logo = models.ImageField(upload_to='casino_logos/')
+    name = models.CharField(max_length=100)
+
+class Bonus(models.Model):
+    casino = models.ForeignKey('Casino', on_delete=models.CASCADE)
+    promo_code = models.CharField(max_length=50)
+    referral_link = models.URLField()
+    website = models.ForeignKey('Sites', on_delete=models.CASCADE)
+
+class Content(models.Model):
+    site = models.ForeignKey('Sites', on_delete=models.CASCADE)
+    text = models.TextField()
+    category = models.CharField(max_length=100)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    keywords = models.CharField(max_length=255)
