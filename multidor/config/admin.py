d383381a6e4119e30e0d323afe7410bc5412ae55
Aidar <aidar.89@bk.ru>
2024-03-19 19:48:17 +0300

    fix

--- multidor/config/admin.py
+++ multidor/config/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from .models import Sites, Content, Bonus, Casino
 
-# Register your models here.
+class BonusAdmin(admin.ModelAdmin):
+    list_display = ('name', 'casino', 'promo_code', 'referral_link', 'website') # Перечислите поля, которые вы хотите отображать в списке бонусов
+
+    def casino_name(self, obj):
+        # Получаем название казино для данного бонуса
+        return obj.casino.name if obj.casino else "-"  # Предположим, что поле в модели Bonus для казино называется "casino", а у казино есть поле "name"
 
 admin.site.register(Sites)
 admin.site.register(Content)
-admin.site.register(Bonus)
+admin.site.register(Bonus, BonusAdmin)
 admin.site.register(Casino)
