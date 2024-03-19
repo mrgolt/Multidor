477f4fed8a2ca5ab1b657dc25dc11bd13ef58a3c
Aidar <aidar.89@bk.ru>
2024-03-19 20:28:05 +0300

    fix

--- multidor/config/models.py
+++ multidor/config/models.py
@@ -12,6 +12,7 @@ class Sites(models.Model):
     hero_image = models.ImageField(upload_to='img/', default='hero.jpg')
     apk_file = models.FileField(upload_to='file/', default='default_apk.apk')
     promo_image = models.ImageField(upload_to='img/', default='promo.jpg')
+    favicon = models.ImageField(upload_to='img/', default='favicon.ico')
     yt_link = models.TextField(blank=True, default='')
     tlg_link = models.TextField(blank=True, default='https://t.me/play_sweet_bonanza')
 
