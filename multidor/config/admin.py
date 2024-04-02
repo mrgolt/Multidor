8ab54bb46ff370169d393df74347855f9c13b3fa
Aidar <aidar.89@bk.ru>
2024-04-02 15:11:01 +0300

    fix

--- multidor/config/admin.py
+++ multidor/config/admin.py
@@ -3,6 +3,7 @@ from .models import *
 from django.db.models import Count
 from django.db.models.functions import TruncDate
 from django.utils.translation import gettext_lazy as _
+from django.utils.html import format_html
 
 class BonusAdmin(admin.ModelAdmin):
     list_display = ('name', 'casino', 'promo_code', 'referral_link', 'website', 'is_active') # Перечислите поля, которые вы хотите отображать в списке бонусов
@@ -17,13 +18,20 @@ class ContentAdmin(admin.ModelAdmin):
         return obj.casino.name if obj.casino else "-"
 
 class SitesAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slot_name', 'allowed_domain', 'num_content', 'site_id', 'provider_name', 'template_name')
+    list_display = ('name', 'slot_name', 'allowed_domain_link', 'num_content', 'site_id', 'provider_name', 'template_name')
     def num_content(self, obj):
         return obj.content_set.count()  # Подсчитываем количество объектов Content, связанных с текущим экземпляром Sites
 
     num_content.short_description = 'Number of Content'
     def casino_name(self, obj):
         return obj.casino.name if obj.casino else "-"
+
+    def allowed_domain_link(self, obj):
+        if obj.allowed_domain:
+            return format_html('<a href="{0}" target="_blank">{0}</a>', obj.allowed_domain)
+        return "-"
+
+    allowed_domain_link.short_description = 'Allowed Domain'
 
 class RedirectAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'target_url', 'visits')
