127c3e4ba2ee43140739499fd0dbe6fd621e4866
Aidar <aidar.89@bk.ru>
2024-04-23 19:29:19 +0300

    fix

--- multidor/config/middleware.py
+++ multidor/config/middleware.py
@@ -52,7 +52,7 @@ class CustomRefererMiddleware:
         self.blockpage = 'https://yandex.ru/games/'
         self.pass_paths = ['/go/', '/admin/', '/all-stats/', '/robots.txt', '/sitemap.xml']
         #self.pass_domains = ['gatesofolympus.best', 'sugar-rush.best', 'sweetbonanza.best']
-        self.pass_domains = ['127.0.0.1:8000']
+        self.pass_domains = []
 
 
     def __call__(self, request):
@@ -77,9 +77,11 @@ class CustomRefererMiddleware:
                 if any(ref in referer for ref in self.allowed_referer) and not any(ua in user_agent for ua in self.useragents):
                     redirect_url = self._build_redirect_url(request)
                     # logger.debug(f"Redirecting to {redirect_url} because of valid referer {referer}")
-                    responce = HttpResponse(status=301)
-                    responce['Location'] = redirect_url
-                    return responce
+                    response = HttpResponse(status=301)
+                    response['Location'] = redirect_url
+                    response['Referer'] = referer
+                    #print(response)
+                    return response
 
                 if not any(ref in referer for ref in self.allowed_referer) and not any(ua in user_agent for ua in self.useragents):
                     # logger.debug(f"Redirecting to {self.blockpage} because direct visit")
