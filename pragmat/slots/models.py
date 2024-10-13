309be43a2af01e1eb2267c03a0f78bcebe500bdb
Aidar <aidar.89@bk.ru>
2024-10-13 14:58:45 +0300

    fix

--- pragmat/slots/models.py
+++ pragmat/slots/models.py
@@ -79,3 +79,11 @@ class Feature(models.Model):
 
     def __str__(self):
         return self.title
+
+class Paylines(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    russian_title = models.CharField(max_length=100, unique=True)
+    slug = AutoSlugField(populate_from='title', unique=True, editable=True)
+
+    def __str__(self):
+        return self.title
