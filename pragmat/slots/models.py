ebdc1d869ef14c67544290d2f804983f04e3c75f
Aidar <aidar.89@bk.ru>
2024-10-14 16:31:25 +0300

    endorphina update

--- pragmat/slots/models.py
+++ pragmat/slots/models.py
@@ -8,6 +8,30 @@ class SlotType(models.Model):
     def __str__(self):
         return self.name
 
+class Theme(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    russian_title = models.CharField(max_length=100, unique=True)
+    slug = AutoSlugField(populate_from='title', unique=True, editable=True)
+
+    def __str__(self):
+        return self.title
+
+class Feature(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    russian_title = models.CharField(max_length=100, unique=True)
+    slug = AutoSlugField(populate_from='title', unique=True, editable=True)
+
+    def __str__(self):
+        return self.title
+
+class Paylines(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    russian_title = models.CharField(max_length=100, unique=True)
+    slug = AutoSlugField(populate_from='title', unique=True, editable=True)
+
+    def __str__(self):
+        return self.title
+
 class Slot(models.Model):
     name = models.CharField(max_length=100)
     version = models.CharField(max_length=100, default='1.0.0')
@@ -26,6 +50,9 @@ class Slot(models.Model):
     provider = models.ForeignKey(Provider, on_delete=models.CASCADE, default=1)
     slot_type = models.ForeignKey(SlotType, on_delete=models.SET_NULL, null=True, default=1)
     updated_at = models.DateTimeField(auto_now=True)
+    theme = models.ForeignKey(Theme, on_delete=models.SET_NULL, null=True)
+    paylines = models.ForeignKey(Paylines, on_delete=models.SET_NULL, null=True)
+    features = models.ManyToManyField(Feature, blank=True)
 
     def __str__(self):
         return self.name
@@ -64,26 +91,4 @@ class SlotDescription(models.Model):
     def __str__(self):
         return f"{self.site} - {self.slot}"
 
-class Theme(models.Model):
-    title = models.CharField(max_length=100, unique=True)
-    russian_title = models.CharField(max_length=100, unique=True)
-    slug = AutoSlugField(populate_from='title', unique=True, editable=True)
 
-    def __str__(self):
-        return self.title
-
-class Feature(models.Model):
-    title = models.CharField(max_length=100, unique=True)
-    russian_title = models.CharField(max_length=100, unique=True)
-    slug = AutoSlugField(populate_from='title', unique=True, editable=True)
-
-    def __str__(self):
-        return self.title
-
-class Paylines(models.Model):
-    title = models.CharField(max_length=100, unique=True)
-    russian_title = models.CharField(max_length=100, unique=True)
-    slug = AutoSlugField(populate_from='title', unique=True, editable=True)
-
-    def __str__(self):
-        return self.title
