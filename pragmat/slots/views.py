af5418cda818f28b3caa690bbe5e975096969203
Aidar <aidar.89@bk.ru>
2024-10-15 15:26:50 +0300

    endorphina

--- pragmat/slots/views.py
+++ pragmat/slots/views.py
@@ -101,7 +101,7 @@ def slot_detail(request, slug):
     # Проверяем, если провайдер Hacksaw
     if slot.provider.id == 2:
         # Проверяем, прошло ли больше 3 дней с момента последнего обновления
-        if timezone.now() - slot.updated_at > timedelta(days=3):
+        if timezone.now() - slot.updated_at > timedelta(days=7):
             user_agents = [
                 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
                 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1.1 Safari/605.1.15',
