bdf809773749ad1404161ae07e1e1bfde1283647
Aidar <aidar.89@bk.ru>
2024-10-31 19:07:33 +0300

    fix

--- pragmat/pragmatic/middleware.py
+++ pragmat/pragmatic/middleware.py
@@ -30,7 +30,7 @@ class CustomRefererMiddleware:
         self.allowed_referer = ['yandex', '127.0.0.1:8000', 'dzen', 'google', 'bing', 'ya.ru', 'mail.ru', 't.me']
         self.useragents = ['yandex', 'google']
         self.blockpage = 'https://yandex.ru/games/'
-        self.pass_paths = ['/go/', '/admin/', '/robots.txt', '/sitemap.xml', '/media/', '/static/']
+        self.pass_paths = ['/go/', '/admin/', '/robots.txt', '/sitemap', '/media/', '/static/', '/play/', '/promo/']
         self.pass_domains = ['127.0.0.1:8000']
 
 
