3600f097a5a79decc8487e2efb8f6aa780a556d3
Айдар <aidar.89@bk.ru>
2024-11-30 14:39:50 +0300

    fix

--- pragmat/pragmatic/middleware.py
+++ pragmat/pragmatic/middleware.py
@@ -19,7 +19,7 @@ def get_site(request: HttpRequest) -> Site:
     domain = request.META.get('HTTP_HOST', '')
 
     if domain == '127.0.0.1:8000':
-        domain = 'pragmatic-play.club'
+        domain = 'pragmatic-provider.site'
     else:
         domain = '.'.join(domain.split('.')[-2:])
 
