e2c2f3bcd00dd947dc1707ad1a0100cd110b3a46
Айдар <aidar.89@bk.ru>
2024-12-03 12:46:12 +0300

    fix

--- multidor/config/views.py
+++ multidor/config/views.py
@@ -115,6 +115,7 @@ def custom_serve(request, slug=None):
 
         if content.first().interface_image:
             site.interface_image = content.first().interface_image
+            site.interface_image_webp = content.first().interface_image
 
         comments = Comment.objects.filter(page=content[0], created_at__lte=timezone.now()).order_by('-created_at')
 
