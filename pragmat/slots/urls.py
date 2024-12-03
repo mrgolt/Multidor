6b9dd534e822d6475afe2f6d0adb359afa25f7df
Айдар <aidar.89@bk.ru>
2024-12-03 18:31:03 +0300

    fix

--- pragmat/slots/urls.py
+++ pragmat/slots/urls.py
@@ -6,6 +6,6 @@ from .views import SlotViewSet, slot_list, slot_detail, optimized_logo
 
 urlpatterns = [
     path('', slot_list, name='slot_list'),
-    path('optimized_logo/<slug:slug>/', optimized_logo, name='optimized_logo'),
+    path('optimized_logo/<slug:slug>.webp', optimized_logo, name='optimized_logo'),
     path('<slug:slug>/', slot_detail, name='slot_detail'),
 ]
