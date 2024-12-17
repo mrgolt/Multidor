d53eb847b6c1931708d95fce03a689508cdc0aaf
Айдар <aidar.89@bk.ru>
2024-12-17 22:37:48 +0300

    fix

--- pragmat/pragmatic/views.py
+++ pragmat/pragmatic/views.py
@@ -212,7 +212,8 @@ def get_proxy(request):
     return HttpResponse(proxy_string)
 
 def get_site(request):
-    response = requests.get('https://infinitysoftcombine-v3.ru/task/get_site_ipv6.php?password=5b5ed5cdc7a466abd74836a514537a53&cat_string=', verify=False)
+    #response = requests.get('https://infinitysoftcombine-v3.ru/task/get_site_ipv6.php?password=5b5ed5cdc7a466abd74836a514537a53&cat_string=', verify=False)
+    response = requests.get('http://135.181.231.90:8998/api/urls?api_key=3f5e1c2a4b8d9e0f1a2b3c4d5e6f7a8b', verify=False)
     return HttpResponse(response.content)
 
 def get_key(request):
